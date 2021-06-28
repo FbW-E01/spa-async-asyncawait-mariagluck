27fb0c5ff887e73eaa1ebccd1e9bcf1fdc496dfc
mariagluck <mariamart.berlin@gmail.com>
2021-06-28 16:13:01 +0200

    finished!

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,23 +2,46 @@ import "../styles/main.scss";
 import "babel-polyfill"
 
 const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
+function promiseOfModal() {
+    new Promise(function (resolve) {
+        window.setTimeout(function () {
+            resolve(modal)
+        }, (1000 * 60));
+    });
+} 
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+const runModal = async () => {
+     await promiseOfModal();     
+     modal.style.display = "block";
+};
+runModal(); 
+
+
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
+    switch (e.target.className) {
         case "close":
         case "modal":
-        modal.style.display = "none"
-        break;
+            modal.style.display = "none";
+            break;
     }
 })
+
+//// continue button animation with alert to subscribe
+const continueBtn = document.getElementById("continue");
+function runAnimation() {
+    return new Promise((resolve) => {
+        continueBtn.addEventListener("animationend", () => {
+        continueBtn.classList.add("btn-info");
+        resolve(continueBtn);
+        });
+    });
+}
+
+async function runAlert() {
+    await runAnimation();
+    alert("Continue to subscribe!");
+}
+
+runAlert();
 
